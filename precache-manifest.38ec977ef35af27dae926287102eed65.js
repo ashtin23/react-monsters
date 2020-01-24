9faf519cf9cd90d11800b9b065d18053c6d4f62a
ashtinlatting <rlatting92@gwu.edu>
2020-01-23 20:08:40 -0500

    Updates

---
+++ precache-manifest.38ec977ef35af27dae926287102eed65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92e8aa6e32e769c8ff10a4de827fc9df",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "a39de2ee55d0d2adb863",
+    "url": "/react-monsters/static/css/main.e730ea17.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/react-monsters/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-monsters/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a39de2ee55d0d2adb863",
+    "url": "/react-monsters/static/js/main.e0e40ee7.chunk.js"
+  },
+  {
+    "revision": "1f77a713eecd4f9ac981",
+    "url": "/react-monsters/static/js/runtime-main.0fa9d154.js"
+  }
+]);
